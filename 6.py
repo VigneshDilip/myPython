d8df46120d5c263e376593b3d883f82a10a3ddb2
VigneshDilip <55728321+VigneshDilip@users.noreply.github.com>
2019-10-15 19:04:37 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,16 @@
+//converts an integer to a string
+num=7
+print(str(num)+" I like that")
+
+
+from math import *
+num=-7
+print(abs(num))
+print(pow(2,3))
+print(max(4,7))
+print(min(1,0))
+print(round(3.2))
+print(round(5.8))
+print(floor(4.7))
+print(ceil(2.1))
+print(sqrt(49))
