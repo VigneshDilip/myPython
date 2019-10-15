87c5c1199ab934df6cddd15a17f8c14b4045ff2e
VigneshDilip <55728321+VigneshDilip@users.noreply.github.com>
2019-10-15 19:03:19 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,8 @@
+name="DiLiP ViGnEsH"
+print(name.lower())
+print(name.upper().islower())
+print(len(name))
+print(name[1])
+print(name.index(“G”))
+print(name.index("ViGn"))
+print(name.replace("DiLiP","YO"))
