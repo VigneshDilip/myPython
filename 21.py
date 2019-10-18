e7b90e3145563e1141c7a3cffa7a96bc91a7cded
VigneshDilip <55728321+VigneshDilip@users.noreply.github.com>
2019-10-18 17:05:57 +0530

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,7 @@
+#Madlib game
+color=input(" Enter a color ")
+noun=input("Enter a noun ")
+name=input("Enter a name ")
+print("Roses are "+color)
+print(noun+" are blue")
+print("My love is true "+name)
