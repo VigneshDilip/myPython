e9fd6c5c795178c67e06a94552b8557f5d952094
VigneshDilip <55728321+VigneshDilip@users.noreply.github.com>
2019-10-18 17:30:29 +0530

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,8 @@
+friends=["Dilip","Vignesh","Esarapu","Disha","Patani"]
+print(friends)
+print(friends[0])
+print(friends[-1])
+print(friends[-2])
+print(friends[1:])
+friends[1]="Hello"
+print(friends[1:4])
