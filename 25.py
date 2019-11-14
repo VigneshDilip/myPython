91b7ca387dddcd39d9c7fe6aeaed06f1fa03e095
VigneshDilip <55728321+VigneshDilip@users.noreply.github.com>
2019-11-14 10:55:12 +0530

    Create 25.py

---
+++ 25.py
@@ -0,0 +1,13 @@
+t1 = ('dilip', 'e') 
+t2 = ('vignesh', 1) 
+  
+if (cmp(t1, t2) != 0): 
+
+    print('Not equal') 
+else: 
+    print('Same') 
+print ('Maximum element in tuples 1,2: ' + 
+        str(max(t1)) +  ',' + 
+        str(max(t2))) 
+print ('Minimum element in tuples 1,2: ' + 
+     str(min(t1)) + ','  + str(min(t2))) 
