c0d00110c3609781cbe59b4f8a2573a0a64684ea
VigneshDilip <55728321+VigneshDilip@users.noreply.github.com>
2019-11-16 15:57:56 +0530

    Create 36.py

---
+++ 36.py
@@ -0,0 +1,13 @@
+#THIS_CODE_IS_TO_EMPTY_THE_DICTIONARY
+
+#METHOD 1 USING clear
+mydict={
+"name":"dilip",
+"state":"kerala",
+"year":2020
+}
+print("old dict")
+print(mydict)
+print("new dict")
+mydict.clear()
+print(mydict)
