f2d33c10598eaede95142bf2f4f9d88c7bbb1605
VigneshDilip <55728321+VigneshDilip@users.noreply.github.com>
2019-11-16 15:42:01 +0530

    Create 31.py

---
+++ 31.py
@@ -0,0 +1,11 @@
+#THIS_CODE_IS_TO_ADD_ITEMS_IN_THE_DICTIONARY
+mydict={
+"name":"dilip",
+"branch":"ece",
+"year":2020
+}
+print("old dict")
+print(mydict)
+print("new dict")
+mydict["year"]=2000
+print(mydict)
