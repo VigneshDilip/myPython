892c3dfbe550527421b645923c022c6cd8c1105d
VigneshDilip <55728321+VigneshDilip@users.noreply.github.com>
2019-11-16 16:19:17 +0530

    Create 39.py

---
+++ 39.py
@@ -0,0 +1,21 @@
+#CREATE THREE DICTIONARIES ,THAN CREATE ONE DICTIONARY THAT WILL CO9NTAIN THE OTHER THREE DICTIONARIES:
+name1={
+  "name":"dilip",
+  "year":2020,
+  "salary":200000
+}
+name2={
+  "name":"vignesh",
+  "branch":"ece"
+}
+name3={
+  "name":"hrithik",
+  "occupation":"actor"
+}
+mybook={
+  "name1":name1,
+  "name2":name2,
+  "name3":name3
+}
+print(mybook)
+
