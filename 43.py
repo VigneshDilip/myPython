b33e4bbcbff2c5e2e994067f57665cbf82feb20b
VigneshDilip <55728321+VigneshDilip@users.noreply.github.com>
2019-11-17 18:43:53 +0530

    Create 43.py

---
+++ 43.py
@@ -0,0 +1,18 @@
+y=0
+b=0
+r=0
+s=0
+while(y<1|b<2|r<3):
+  y=int(input("Enter a number"))
+  b=int(input("Enter a number"))
+  r=int(input("Enter a number"))
+i=1
+j=2
+k=3
+while(i<=y & j<=b & k<=r) :
+  
+  s=i+j+k
+  i=i+1
+  j=j+1
+  k=k+1
+print(s)
